8a7de97f5c18c4288d5f3eebd734c897bf58eafc
Salvador <gonzalezlsalvador@outlook.com>
2020-04-07 02:43:15 -0700

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './limpiea-california-logo.png';
 import './App.css';
+
+const divStyle = {
+  backgroundColor: '#F2F2F2',
+};
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-md navbar-light" style={divStyle} id="main-navigation">
+        <a href="#">
+          <img src={logo} id="logo-limpieza"></img>
         </a>
-      </header>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse text-center" id="navbarNavAltMarkup">
+          <div className="navbar-nav center text-center">
+            <a className="nav-item nav-link active" href="#">Inicio <span className="sr-only">(current)</span></a>
+            <a className="nav-item nav-link" href="#">Servicios</a>
+            <a className="nav-item nav-link" href="#">Contacto</a>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
